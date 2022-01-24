e816a0f0ac3cefe3fc8383446f62b7bc919499a8
王明晓 <wangmingxiao@wangmingxiaodeMacBook-Air.local>
2022-01-25 00:48:37 +0800

    add image in first page

--- learning-resources-center/src/App.js
+++ learning-resources-center/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import logo from './bookMuseum.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-      Hi, Mingxiao. Welcome to Your Resources Center!
+      <header className="App-headeßr">
+        <img style={{width:"100%",height:"100%"}} src={logo}  alt="logo" />
+        <p>
+          Hi, Mingxiao. Welcome to Your Resources Center!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
